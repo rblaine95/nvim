2b6c50e2bb5853cba5c74e4fc0f3447fc7358d07
Robbie Blaine <robbie@zenaptix.com>
2023-01-27 15:13:29 +0200

    Initial config
    * gruvbox theme
    * gitcommit columns at `50` and `72`
    * general columns at `80`, `100`, and `120`
    * `rust-analyzer` lsp
    * github copilot

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+    vim.opt_local.cc = "50,72"
+  end,
+})
